d7de47b402e8b3e4d66f8b2240d148574817568e
Aimar Kiivits <Aimarkiivits1@gmail.com>
2024-06-17 19:16:40 +0300

    Create new Todo from user data

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { createTodo } from "../controllers/todos";
 
 const router = Router();
 
+router.post('/', createTodo);
 router.get('/');
-router.post('/');
 router.patch('/:id');
 router.delete('/:id');
 
